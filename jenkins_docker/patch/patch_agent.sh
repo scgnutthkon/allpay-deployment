18d5cb72e06a9821cd1c9fdcd3b26091a599a6e2
Nutthawut Kongsopa <nutthkon@scg.com>
2024-06-15 11:54:52 +0700

    update patch jenkins agent

--- jenkins_docker/patch/patch_agent.sh
+++ jenkins_docker/patch/patch_agent.sh
@@ -6,8 +6,10 @@ fi
 
 cat << EOF > /etc/systemd/system/docker.service.d/http-proxy.conf
 [Service]
-Environment="HTTP_PROXY=http://CADAllpayVendor02:Avd%%40%%400213579@proxy-server.scg.com:3128/"
-Environment="HTTPS_PROXY=http://CADAllpayVendor02:Avd%%40%%400213579@proxy-server.scg.com:3128/"
+Environment="HTTP_PROXY=http://10.100.12.240:6969/"
+Environment="HTTPS_PROXY=http://10.100.12.240:6969/"
+# Environment="HTTP_PROXY=http://CADAllpayVendor02:Avd%%40%%400213579@proxy-server.scg.com:3128/"
+# Environment="HTTPS_PROXY=http://CADAllpayVendor02:Avd%%40%%400213579@proxy-server.scg.com:3128/"
 Environment="NO_PROXY=127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,*.scg.com"
 EOF
 
@@ -25,8 +27,8 @@ cat << EOF > /home/jenkins/.docker/config.json
 {
  "proxies": {
     "default": {
-        "httpProxy": "http://CADAllpayVendor01:Avd%40%400113579@proxy-server.scg.com:3128/",
-        "httpsProxy": "http://CADAllpayVendor01:Avd%40%400113579@proxy-server.scg.com:3128/",
+        "httpProxy": "http://10.100.12.240:6969/",
+        "httpsProxy": "http://10.100.12.240:6969/",
         "noProxy": "127.0.0.1,10.100.12.94,10.100.12.93"
     }
  }
@@ -40,5 +42,5 @@ chmod 664 /home/jenkins/.docker/config.json
 
 echo "Config git for using proxy"
 
-su -c "git config --global http.proxy http://CADAllpayVendor01:Avd%40%400113579@proxy-server.scg.com:3128/" jenkins
-su -c "git config --global https.proxy http://CADAllpayVendor01:Avd%40%400113579@proxy-server.scg.com:3128/" jenkins
+su -c "git config --global http.proxy http://10.100.12.240:6969/" jenkins
+su -c "git config --global https.proxy http://10.100.12.240:6969/" jenkins
