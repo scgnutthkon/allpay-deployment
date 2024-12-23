683e5e68438202712ba023673e91dfa97b43b5f9
Nutthawut Kongsopa <nutthkon@scg.com>
2024-12-23 21:56:18 +0700

    script to install postgresql

---
+++ scripts/initial_postgresql_primarywithreplica_server.sh
@@ -0,0 +1,53 @@
+export https_proxy=http://CADAllpayVendor05:Avd%40%400513579@proxy-server.scg.com:3128/
+replica_user=replica_user
+replica_password=Pay@1234AB!
+replica_port=5433
+
+sudo apt update
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+
+sudo apt update
+
+sudo apt install postgresql-17 postgresql-contrib -y
+
+sudo cat << EOF > /etc/postgresql/17/main/conf.d/override.conf
+listen_addresses = '*'
+port = '5432'
+wal_level = 'replica'
+EOF
+
+echo "host    replication     $replica_user      0.0.0.0/0            md5" >> /etc/postgresql/17/main/pg_hba.conf
+echo "host    replication     $replica_user      ::/0            md5" >> /etc/postgresql/17/main/pg_hba.conf
+echo "host     all             all             0.0.0.0/0               md5" >> /etc/postgresql/17/main/pg_hba.conf
+echo "host     all             all             ::/0                    md5" >> /etc/postgresql/17/main/pg_hba.conf
+
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+
+sudo -u postgres psql -p 5432 -c "ALTER USER postgres WITH PASSWORD '$postgres_password';"
+sudo -u postgres psql -p 5432 -c "CREATE ROLE $replica_user WITH REPLICATION LOGIN PASSWORD '$replica_password';"
+
+sudo systemctl restart postgresql
+
+##############Create Replica##########################
+
+sudo pg_createcluster 17 replica --port=$replica_port
+
+sudo cat << EOF > /etc/postgresql/17/replica/conf.d/override.conf
+listen_addresses = '*'
+port = '$replica_port'
+EOF
+
+echo "host     all             all             0.0.0.0/0               md5" >> /etc/postgresql/17/replica/pg_hba.conf
+echo "host     all             all             ::/0                    md5" >> /etc/postgresql/17/replica/pg_hba.conf
+
+sudo systemctl enable postgresql
+
+sudo rm -rf /var/lib/postgresql/17/replica/*
+
+PGPASSWORD="$replica_password" sudo pg_basebackup -h $primary_host -U $replica_user -X stream -C -S replica_1 -v -R -W -D /var/lib/postgresql/17/replica/
+
+sudo systemctl restart postgresql
+
---
+++ scripts/initial_postgresql_replica_server.sh
@@ -0,0 +1,30 @@
+export https_proxy=http://CADAllpayVendor05:Avd%40%400513579@proxy-server.scg.com:3128/
+replica_user=replica_user
+replica_password=Pay@1234AB!
+primary_host=10.101.3.156
+
+sudo apt update
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+
+sudo apt update
+
+sudo apt install postgresql-17 postgresql-contrib -y
+
+sudo cat << EOF > /etc/postgresql/17/main/conf.d/override.conf
+listen_addresses = '*'
+port = '5432'
+EOF
+
+echo "host     all             all             0.0.0.0/0               md5" >> /etc/postgresql/17/main/pg_hba.conf
+echo "host     all             all             ::/0                    md5" >> /etc/postgresql/17/main/pg_hba.conf
+
+sudo systemctl enable postgresql
+sudo systemctl stop postgresql
+
+sudo rm -rf /var/lib/postgresql/17/main/*
+
+PGPASSWORD="$replica_password" sudo pg_basebackup -h $primary_host -U $replica_user -X stream -C -S replica_1 -v -R -W -D /var/lib/postgresql/17/main/
+
+sudo systemctl start postgresql
