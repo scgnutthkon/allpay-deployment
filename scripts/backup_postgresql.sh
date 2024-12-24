3b024ed1165d1e84d891c9f1133d83a81973ba44
Nutthawut Kongsopa <nutthkon@scg.com>
2024-12-24 10:17:13 +0700

    Update backup_postgresql.sh

--- scripts/backup_postgresql.sh
+++ scripts/backup_postgresql.sh
@@ -7,10 +7,11 @@ DATE=$(TZ="Asia/Bangkok" date '+%Y%m%d%H%M')
 
 # Create backup directory if it doesn't exist
 sudo mkdir -p $BACKUP_DIR
+sudo chmod 777 $BACKUP_DIR
 
 # Backup each database
 for DB_NAME in "${DB_NAMES[@]}"; do
-   sudo -u postgres pg_dump --clean --if-exists --load-via-partition-root --quote-all-identifiers ${DB_NAME} | sudo gzip > ${BACKUP_DIR}/${DB_NAME}-${DATE}.sql.gz
+   sudo -u postgres bash -c "pg_dump --clean --if-exists --load-via-partition-root --quote-all-identifiers ${DB_NAME} | gzip > ${BACKUP_DIR}/${DB_NAME}-${DATE}.sql.gz"
 done
 
 find ${BACKUP_DIR}/ -type f -name "*.gz" -mtime +10 -exec rm -f {} \;
