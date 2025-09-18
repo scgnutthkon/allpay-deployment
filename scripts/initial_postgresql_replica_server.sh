306aa37e1f5ebfbcd3868a3040abca80dde39ea2
Nutthawut Kongsopa <nutthkon@scg.com>
2025-09-18 13:02:16 +0700

    fix script creat pg replica

--- scripts/initial_postgresql_replica_server.sh
+++ scripts/initial_postgresql_replica_server.sh
@@ -1,16 +1,29 @@
-export https_proxy=http://CADAllpayVendor05:Avd%40%400513579@proxy-server.scg.com:3128/
 replica_user=replica_user
 replica_password=Pay@1234AB!
 primary_host=10.101.3.156
+replica_port=5432
+
+proxyUrl="http://CADAllpayVendor05:Avd%40%400513579@proxy-server.scg.com:3128/"
+proxyEsp=$(echo $proxyUrl | sed s/%/%%/g)
+
+export http_proxy="$proxyUrl"
+export https_proxy="$proxyUrl"
+export ftp_proxy="$proxyUrl"
+export no_proxy=127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,.scg.com,.scg-mpc.hpe.com
 
 sudo apt update
+sudo apt install postgresql-common
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 sudo apt update
 
 sudo apt install postgresql-17 postgresql-contrib -y
+
+sudo pg_createcluster 17 main --start
+
 
 # Configure the replica PostgreSQL instance
 sudo bash -c "cat << EOF > /etc/postgresql/17/main/conf.d/override.conf
@@ -38,10 +51,12 @@ sudo systemctl enable postgresql
 sudo rm -rv /var/lib/postgresql/17/main
 
 # Perform a base backup from the primary
-PGPASSWORD="$replica_password" sudo -u postgres pg_basebackup -h "$primary_host" -U "$replica_user" -X stream -C -S replica_1 -v -R -D /var/lib/postgresql/17/main/
+PGPASSWORD="$replica_password" sudo -u postgres pg_basebackup -h "$primary_host" -U "$replica_user" -X stream -C -S replica_2 -v -R -D /var/lib/postgresql/17/main/
 
 # Set proper ownership for the replica directory
 sudo chown -R postgres:postgres /var/lib/postgresql/17/main
+
+sudo pg_ctlcluster 17 main restart
 
 # Restart PostgreSQL to finalize setup
 sudo systemctl restart postgresql
